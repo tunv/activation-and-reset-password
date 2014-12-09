c1d95a2cba583d6e743f5b5dd21063cb011a5f26
tunv <nguyenvantu.dhbk@gmail.com>
2014-12-09 12:35:17 +0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 	validates :email, presence: true,length: {maximum:255},format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	has_secure_password
 	validates :password, presence:true ,length: {minimum:6}
+	 validates :password, length: { minimum: 6 }, allow_blank: true
+
 
 	 def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
